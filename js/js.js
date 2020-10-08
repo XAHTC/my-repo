574edfb0b76be0066d1c402b42abd0e8926ff022
Alex <tiamat1288@gmail.com>
2020-10-08 23:09:42 +0300

    added animation + fixed some bugs

--- js/js.js
+++ js/js.js
@@ -1,6 +1,55 @@
-let menuBody = document.querySelector(".menu__body");
-let iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+const iconMenu = document.querySelector(".menu__icon");
 iconMenu.addEventListener('click', () => {
   menuBody.classList.toggle("active");
 });
-;
+
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', (event) => {
+    event.preventDefault();
+    const blockId = anchor.getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+};
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      } else {
+        if (!animItem.classList.contains('_anim-no-hide')) {
+          animItem.classList.remove('_active');
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+  animOnScroll();
+}
